4340619ed23f45ab3bad4a2d9376fc5313c26624
Cljones2 <98124883+Cljones2@users.noreply.github.com>
2024-12-12 21:26:11 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'CJ'
-description 'First join marker and starter car script for ND Core'
+author 'Raxons server'
+description 'First starter car script for ND Core'
 version '1.0.0'
 
 server_script 'server.lua'
